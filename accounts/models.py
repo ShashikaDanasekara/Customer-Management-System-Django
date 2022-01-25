370c669973b885ef19d8ed8a2a37983ea661bd87
ShashikaDanasekara <shashika.danasekara@outlook.com>
2022-01-25 10:36:45 +0530

    basic web app

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,48 @@
+from sre_parse import CATEGORIES
+from tkinter import CASCADE
+from tkinter.tix import STATUS
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=200,null=True)
+    phone = models.CharField(max_length=200,null=True)
+    email = models.CharField(max_length=200,null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200,null=True)
+    
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    CATEGORIES = (
+        ('Indoors','Indoors'),
+        ('Outdoor','Outdoor'),
+        ('Sports','Sports'),
+    )
+    name = models.CharField(max_length=200,null=True)
+    price = models.FloatField(max_length=200,null=True)
+    category = models.CharField(max_length=200,null=True,choices=CATEGORIES)
+    description = models.CharField(max_length=200,null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    STATUS = (
+        ('Pending','Pending'),
+        ('Delivered','Delivered'),
+        ('Out of delivary','Out of delivary'),
+    )
+    customer = models.ForeignKey(Customer,blank=True, null=True,on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product,blank=True, null=True,on_delete=models.SET_NULL)
+    status = models.CharField(max_length=200,null=True,choices=STATUS)
+    date_created = models.DateTimeField(auto_now_add=True)
